67455befba81ae27334215888f6b88380a0f99d0
Lucas Silva <lucassilvaee1245@gmail.com>
2021-06-26 10:50:44 -0300

    Ciclo de vida bean @PostConstructor e @destory

--- src/main/java/com/thislucasme/services/AtivacaoClienteService.java
+++ src/main/java/com/thislucasme/services/AtivacaoClienteService.java
@@ -1,6 +1,9 @@
 package com.thislucasme.services;
 
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +18,19 @@ public class AtivacaoClienteService {
 	@Autowired
 	private Notificador notificador;
 	
+	@PostConstruct
+	public void init() {
+		System.out.println("INIT");
+	}
+	@PreDestroy 
+	public void destroy() {
+		System.out.println("DESTROY");
+	}
 	public void ativar(com.thislucasme.model.Cliente cliente) {
 		cliente.ativar();
 		
 			notificador.notificar(cliente, "Seu cadastro no sistema est� ativo");
 		
 	}
+	
 }
