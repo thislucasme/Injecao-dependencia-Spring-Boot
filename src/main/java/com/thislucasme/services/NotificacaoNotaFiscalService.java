bd7d3080d08396f90fbf1c65cfd9e898feab5f13
Lucas Silva <lucassilvaee1245@gmail.com>
2021-06-26 12:03:44 -0300

    Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/thislucasme/services/NotificacaoNotaFiscalService.java
+++ src/main/java/com/thislucasme/services/NotificacaoNotaFiscalService.java
@@ -1,13 +1,20 @@
 package com.thislucasme.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificacaoNotaFiscalService {
 	
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
+	
 	@EventListener
 	public void clienteAtivadoListener(ClienteAtivadoEvent event) {
+		System.out.println("Porta: "+ porta);
 		System.out.println("Emitindo nota fiscal para cliente: "+event.getCliente().nome);
 	}
 	
